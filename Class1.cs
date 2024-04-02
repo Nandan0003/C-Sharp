2daac6e57a226fa49e2108441053d33d8209c60b
Nandan kumar M <nandankumar3m@gmail.com>
2024-04-02 11:54:20 +0530

    class file for prog1
    Class File of program 1 which shows inhereitence in a basic way

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEwAPPForLearning
+{
+    public class FullName
+    {
+        public string Name(string Name)
+        {
+            Console.WriteLine(Name);
+            return Name;
+            
+            
+            
+        
+        }
+    }
+
+    public class NickName : FullName {
+
+        public string Name(string Name)
+        {
+            Console.WriteLine(Name);
+            return Name;
+        }
+        
+    
+    
+    }
+}
