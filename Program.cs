68d4045c2758ec0466c61b062080b67187e4574d
Nandan kumar M <nandankumar3m@gmail.com>
2024-04-02 11:55:26 +0530

    Main file for prog1
    this is the main file where main program exist and where we have called the class

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEwAPPForLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FullName fullName = new FullName();
+            fullName.Name("Nandan");
+        }
+    }
+}
